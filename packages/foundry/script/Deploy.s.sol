2958afb3fd52771660e2e347f23f01354092d211
shijiang.guo <shijiang@shijiangguodeMacBook-Pro.local>
2023-11-19 11:25:12 +0800

    finish deploy script coding

--- packages/foundry/script/Deploy.s.sol
+++ packages/foundry/script/Deploy.s.sol
@@ -1,11 +1,31 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+
+import "../contracts/interfaces/IRealityETH_ERC20.sol";
+import "../contracts/RealityETH_ERC20.sol";
 import "../contracts/RealityETH_v3_0.sol";
+import "../contracts/BettingGameOpenAction.sol";
+import "../contracts/interfaces/IBettingGame.sol";
+import "../contracts/interfaces/IBettingGame_ERC20.sol";
+import "../contracts/BettingGame_ERC20.sol";
+
+import "../contracts/BettingGame.sol";
+
+
 import "./DeployHelpers.s.sol";
 
-contract DeployScript is ScaffoldETHDeploy {
+
+// forge script script/Deployer.s.sol:TreasureDeployer --rpc-url $RPC_URL  --private-key $PRIVATE_KEY --broadcast -vvvv
+contract Deployer is ScaffoldETHDeploy {
     error InvalidPrivateKey(string);
+
+    RealityETH_ERC20 public realityAddress;
+    BettingGame_ERC20 public gameErc20Address;
+    BettingGameOpenAction public bgoaAddress;
+
+    IERC20 public tokenAddress = IERC20(address(0));
 
     function run() external {
         uint256 deployerPrivateKey = setupLocalhostEnv();
@@ -15,18 +35,23 @@ contract DeployScript is ScaffoldETHDeploy {
             );
         }
         vm.startBroadcast(deployerPrivateKey);
-        RealityETH_v3_0 yourContract = new RealityETH_v3_0(
-    );
-        console.logString(string.concat("YourContract deployed at: ", vm.toString(address(yourContract))));
-        vm.stopBroadcast();
+        realityAddress = new RealityETH_ERC20();
+        console.logString(string.concat("RealityETH_ERC20_v3_0 deployed at: ", vm.toString(address(realityAddress))));
 
-        /**
-         * This function generates the file containing the contracts Abi definitions.
-         * These definitions are used to derive the types needed in the custom scaffold-eth hooks, for example.
-         * This function should be called last.
-         */
+        IRealityETH_ERC20 realityErc20Address = IRealityETH_ERC20(address(realityAddress));
+        gameErc20Address = new BettingGame_ERC20(realityErc20Address, tokenAddress);
+        console.logString(string.concat("BettingGame deployed at: ", vm.toString(address(gameErc20Address))));
+
+        // address lensHubProxyContract, address _bettingGameContract, address _realityETH, IERC20 _tokenAddress
+        bgoaAddress = new BettingGameOpenAction(
+            address(0),
+            address(gameErc20Address),
+            address(realityErc20Address),
+            tokenAddress
+        );
+        console.logString(string.concat("BettingGameOpenAction deployed at: ", vm.toString(address(bgoaAddress))));
+
+        vm.stopBroadcast();
         exportDeployments();
     }
-
-    function test() public {}
 }
